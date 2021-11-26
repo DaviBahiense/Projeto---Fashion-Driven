fb65dbfcfb996795157572613530a9c5fc497bf3
Davi Bahiense <davibnovaes@hotmail.com>
2021-11-25 23:44:02 -0300

    interacao dinamica -> confirmar pedido: concluido

--- JavaScript/js.js
+++ JavaScript/js.js
@@ -1,1 +1,50 @@
-const name = prompt("Qual o seu nome?")
+//const name = prompt("Qual o seu nome?")
+
+let arraySelected = []
+
+function selectModel(clic){
+    const model = document.querySelector(".model .selected")
+    if(model){
+        model.classList.remove("selected")
+        arraySelected.pop(1)
+    }
+    clic.classList.add("selected")
+    arraySelected.push(1)
+    verify()}
+function selectCollar(clic){
+    const model = document.querySelector(".collar .selected")
+    if(model){
+        model.classList.remove("selected")
+        arraySelected.pop(1)
+    }
+    clic.classList.add("selected")
+    arraySelected.push(1)
+    verify()}
+function selectTissue(clic){
+    const model = document.querySelector(".tissue .selected")
+    if(model){
+        model.classList.remove("selected")
+        arraySelected.pop(1)
+    }
+    clic.classList.add("selected")
+    arraySelected.push(1)
+    verify()
+}
+
+const button = document.querySelector("button")
+button.style.pointerEvents = 'none'
+
+function verify(){
+    const input = document.querySelector("input")
+    const isWrite = input.value
+    
+    
+if(arraySelected.length === 3 && isWrite !== ""){
+    button.classList.add("disable")
+    button.style.pointerEvents = 'auto'
+}
+}
+function confirm(){
+    alert("Sua encomenda foi efetuada com sucesso")
+}
+
